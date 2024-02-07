17eef02e2eb72f5d2ced173b8c84ba704cc5aba0
ummefahad <rokhaiyasultana97@gmail.com>
2024-02-07 20:02:17 +0530

    jupyter notebook

--- uran.py
+++ uran.py
@@ -44,28 +44,36 @@ if __name__ == '__main__':
         sys.exit(1)
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    remote_server_uri = 'https://dagshub.com/ummefahad/Urban_data_Analysis.mlflow'
-    mlflow.set_tracking_uri(remote_server_uri)
-
-    with mlflow.start_run():
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)  # Fix typo in class name
         lr.fit(train_x, train_y)
 
-        predicted_qualities = lr.predict(test_x)
+        predicted_qualities = lr.predict(test_x)  # Fix variable name here
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
         print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+
         print(" RMSE: %s" % rmse)
         print(" MAE: %s" % mae)
         print(" R2: %s" % r2)
 
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_param("rmse", rmse)
-        mlflow.log_param("mae", mae)
-        mlflow.log_param("r2", r2)
+        mlflow.log_param("rmse", rmse)  # Fix variable name here
+        mlflow.log_param("mae", mae)  # Fix variable name here
+        mlflow.log_param("r2", r2)  # Fix variable name here
+        remote_server_uri = "https://dagshub.com/ummefahad/Urban_data_Analysis.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
-        mlflow.sklearn.log_model(lr, "model")
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name="ElasticnetWineModel"
+            )
+        else:
+            mlflow.sklearn.log_model(lr, "model")
